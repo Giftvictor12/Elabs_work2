8c0ca15d68269c9a68a42365341f69ef5577265e
Giftvictor12 <giftvictor1212@gmail.com>
2023-08-16 00:57:18 +0100

    project on arithmetic functions and arrays

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a project using forEach and Map arrays to show and list items and things and also using functions to create arithmetic functions in Javascript.  
---
+++ functions and arithemetic operations .js
@@ -0,0 +1,49 @@
+// using 4 different arithemetic function
+//which are addition divison subtraction and multiplication 
+
+//Addition 
+
+function sum(a, b) {
+  return a + b;
+}
+let num = sum(10, 5)
+console.log(num);
+
+
+// output 15
+
+
+Subtraction  
+
+function getDifference(a, b) {
+  return a - b;
+}
+let result = getDifference(20, 10)
+console.log(result);
+
+//output 10 
+
+
+
+// Multiplication 
+
+function multiplyThreeNums(Num1, Num2, Num3) {
+  return Num1 * Num2 * Num3
+}
+let output = multiplyThreeNums(5, 4, 2)
+console.log(output);  
+
+//output 40 
+
+
+
+// Division 
+
+function DivideTwoNums(Num0, Num1) {
+ return Num0 / Num1;
+}
+let value = DivideTwoNums(10, 2)
+console.log(value);  
+
+// Output 5 
+
